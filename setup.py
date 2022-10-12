f1e5bbf29a52d47e15cd4f1c84cb1562a66f939f
B260-admin <b260-admin@B260-MB003-WL-2.local>
2022-10-12 16:36:56 +0200

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bicon",
-    version="1.3.3",
+    version="1.3.4",
     author="Olga Lazareva",
     author_email="olga.lazareva@tum.de",
     description="BiCoN - a package for network-constrained biclustering of omics data",
@@ -25,7 +25,7 @@ setuptools.setup(
         'networkx>=2.3',
         'matplotlib>=3.1.0',
         'scipy>=1.3.0',
-        'gseapy>=0.9.15',
+        'gseapy==0.9.15',
         'seaborn>=0.9.0',
         'mygene>=3.1.0',
         'scikit_learn>=0.22',
